6f8862331e7bf4de7eef2857fcedc93efa211492
songsiri <aversebike38917@gmail.com>
2026-04-26 15:17:38 +0900

    Rename 2026/26 04 19 alert.c to visual studio/C/2026/26 04 19 alert.c

--- 2026/26 04 19 alert.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("\a화재가 방샐하였습니다.\a");
-
-
-	return 0;
-}
---
+++ visual studio/C/2026/26 04 19 alert.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\aÈ­Àç°¡ ¹æ»øÇÏ¿´½À´Ï´Ù.\a");
+
+
+	return 0;
+}
